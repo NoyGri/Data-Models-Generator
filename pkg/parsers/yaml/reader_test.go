d3c76b3bd5e85f16b056e4fc092b74e0354a496d
Noy Grisaru <noy@grisaru.com>
2023-08-21 07:18:06 +0300

    GH-2: Reader -> TestInvalidYaml -> EmptyFile

--- pkg/parsers/yaml/reader_test.go
+++ pkg/parsers/yaml/reader_test.go
@@ -2,6 +2,7 @@ package yaml
 
 import (
 	. "github.com/onsi/gomega"
+	"os"
 	"testing"
 )
 
@@ -10,8 +11,29 @@ func TestInvalidYaml(t *testing.T) {
 	t.Run("NoSuchFile", func(t *testing.T) {
 		g := NewWithT(t)
 
+		// Act
 		data, err := Read("no_such_file.yaml")
 
+		// Assert
+		g.Expect(err).NotTo(BeNil())
+		g.Expect(data).To(BeNil())
+	})
+
+	t.Run("EmptyFile", func(t *testing.T) {
+		g := NewWithT(t)
+
+		// Arrange
+		err := os.WriteFile("empty_file.yaml", []byte{}, 0666)
+		g.Expect(err).To(BeNil())
+		defer func() {
+			err := os.Remove("empty_file.yaml")
+			g.Expect(err).To(BeNil())
+		}()
+
+		// Act
+		data, err := Read("empty_file.yaml")
+
+		// Assert
 		g.Expect(err).NotTo(BeNil())
 		g.Expect(data).To(BeNil())
 	})
